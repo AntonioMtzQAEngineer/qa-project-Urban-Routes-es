3c8dd32d8745a29504c2d88645295d717b581359
Antonio Francisco Martínez <250558955+AntonioMtzQAEngineer@users.noreply.github.com>
2026-04-03 19:34:49 -0600

    qa-project-Urban-Routes-es

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://029f5e89-1ed2-457a-8c61-c3ff8b9e57c2.serverhub.tripleten-services.com'
+urban_routes_url = 'https://cnt-8523ffc6-34d0-4c77-9e42-432a3e04754a.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import data
 from selenium import webdriver
 from selenium.webdriver import Keys
@@ -37,21 +39,98 @@ def retrieve_phone_code(driver) -> str:
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    button_round = (By.XPATH, "//button[@class = 'button round']")
+    btn_comfort = (By.XPATH, "//div[@class='tcard-title' and text()='Comfort']")
+    btn_number = (By.XPATH, "//div[@class = 'np-text' and text() = 'Número de teléfono']")
+    set_number = (By.ID, 'phone')
+    btn_next = (By.XPATH, "//div[@class = 'section active']//button[@type = 'submit']")
+    code_msm = (By.ID, "code")
+    button_confirm = (By.XPATH, "//button[text()='Confirmar']")
+    btn_method_pay = (By.CLASS_NAME, "pp-text")
+    btn_add_card = (By.CSS_SELECTOR, "div.pp-row.disabled")
+    fill_number_card = (By.ID, 'number')
+    fill_number_code = (By.ID, 'code')
+    btn_AGREGAR = (By.XPATH, "//button[text()='Agregar']")
+    close_btn = (By.CSS_SELECTOR, "div.payment-picker.open div.modal div.section.active button.close-button.section-close")
+    comment = (By.ID, "comment")
+    slider_round_mant = (By.XPATH,"//div[@class='r-sw-label' and text()='Manta y pañuelos']/following-sibling::div[@class='r-sw']//span[@class='slider round']")
+    counter_value = (By.XPATH,"//div[contains(., 'Helado') and @class='r-counter-container']//div[@class='counter-value']")
+    counter_plus = (By.XPATH,"//div[contains(., 'Helado') and @class='r-counter-container']//div[@class='counter-plus']")
+    btn_get_taxi = (By.CLASS_NAME, "smart-button-main")
+    flag_driver = (By.XPATH, "//div[@class='order-header-title' and contains(text(), 'Buscar automóvil')]")
+
+
 
     def __init__(self, driver):
         self.driver = driver
-
     def set_from(self, from_address):
         self.driver.find_element(*self.from_field).send_keys(from_address)
-
     def set_to(self, to_address):
         self.driver.find_element(*self.to_field).send_keys(to_address)
-
+#Para validar que estén bien escritos
+    def wait_for_element(self, locator,timeout=10):
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.visibility_of_element_located(locator))
+    def wait_clickable(self, locator, timeout=10):
+        return WebDriverWait(self.driver, timeout).until(expected_conditions.element_to_be_clickable(locator))
     def get_from(self):
         return self.driver.find_element(*self.from_field).get_property('value')
-
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
+    def set_route(self, address_from, address_to):
+        self.set_from(address_from)
+        self.set_to(address_to)
+    def click_take_taxi(self):
+        self.driver.find_element(*self.button_round).click()
+    def select_comfort(self):
+        self.driver.find_element(*self.btn_comfort).click()
+    def click_btn_number(self):
+        self.driver.find_element(*self.btn_number).click()
+    def send_number(self):
+        self.driver.find_element(*self.set_number).send_keys(data.phone_number)
+        self.driver.find_element(*self.btn_next).click()
+    def get_code(self):
+        code = retrieve_phone_code(self.driver)
+        self.driver.find_element(*self.code_msm).send_keys(code)
+        self.wait_for_element(self.button_confirm)
+        self.driver.find_element(*self.button_confirm).click()
+
+    def set_debit_card(self):
+        self.driver.find_element(*self.btn_method_pay).click()
+        self.wait_for_element(self.btn_add_card)
+        self.driver.find_element(*self.btn_add_card).click()
+
+        card_field = self.driver.find_element(*self.fill_number_card)
+        card_field.send_keys(data.card_number)
+        card_field.send_keys(Keys.TAB+data.card_code+Keys.TAB)
+
+        self.wait_clickable(self.btn_AGREGAR).click()
+        self.wait_for_element(self.close_btn).click()
+
+    def send_comment(self):
+        message = "Traiga un aperitivo"
+        comment_field = self.driver.find_element(*self.comment)
+        comment_field.send_keys(message)
+
+    def select_request(self):
+        element = self.driver.find_element(*self.slider_round_mant)
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        self.driver.find_element(*self.slider_round_mant).click()
+    def order_gelato(self):
+
+        while True:
+            cnt = self.driver.find_element(*self.counter_value).text
+            counter = int(cnt)
+
+            if counter == 2:
+                break
+            self.driver.find_element(*self.counter_plus).click()
+
+    def wait_driver(self):
+        self.driver.find_element(*self.btn_get_taxi).click()
+        WebDriverWait(self.driver, 50).until_not(expected_conditions.visibility_of_element_located(self.flag_driver))
+
+
+
 
 
 
@@ -62,19 +141,41 @@ class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
         # no lo modifiques, ya que necesitamos un registro adicional habilitado para recuperar el código de confirmación del teléfono
-        from selenium.webdriver import DesiredCapabilities
-        capabilities = DesiredCapabilities.CHROME
-        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        #from selenium.webdriver import DesiredCapabilities
+        #capabilities = DesiredCapabilities.CHROME
+        #capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        #cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        from selenium.webdriver.chrome.options import Options
+        options = Options()
+        options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+        cls.driver = webdriver.Chrome(options=options)
+
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
         routes_page = UrbanRoutesPage(self.driver)
+        routes_page.wait_for_element(routes_page.from_field)
         address_from = data.address_from
         address_to = data.address_to
         routes_page.set_route(address_from, address_to)
+        routes_page.wait_for_element(routes_page.button_round)
+        routes_page.click_take_taxi()
+        routes_page.wait_for_element(routes_page.btn_comfort)
+        routes_page.select_comfort()
+        routes_page.click_btn_number()
+        routes_page.wait_for_element(routes_page.set_number)
+        routes_page.send_number()
+        routes_page.get_code()
+        routes_page.set_debit_card()
+        routes_page.send_comment()
+        routes_page.select_request()
+        routes_page.order_gelato()
+        routes_page.wait_driver()
+
+        time.sleep(5)
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
+
 
 
     @classmethod
